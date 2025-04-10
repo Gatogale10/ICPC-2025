33eb30253a17d1871b541248b0b7a6a1527bc59b
Gatogale10 <jorgegael235@gmail.com>
2025-04-10 17:46:02 -0600

    Que cosas o herrramientas son mas eficientes para usar en´c++

---
+++ HerramientasICPC/FuncionalidadVector.c
@@ -0,0 +1,14 @@
+#include<cstudio>
+#include<vector>
+
+
+int main(){
+//Aqui vamos a poner algunas de la funcionalidades y maneras de usar un vector en c++
+
+//Lo declaramos de la siguiente manera
+vector<int> v;
+//Si queremos declaralos con valores seria asi
+vector<int> v(5, 5);
+
+
+return 0;}
---
+++ HerramientasICPC/FuncionalidadVector.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+#include <vector>
+
+using namespace std;
+
+
+
+int main(){
+//Aqui vamos a poner algunas de la funcionalidades y maneras de usar un vector en c++
+
+//Lo declaramos de la siguiente manera, decimos su tamaño inicial
+    vector<int> v(5);
+//Si queremos declaralos con valores seria asi
+    vector<int> v(5,5) //Se llena con 5s
+
+
+
+return 0;}
